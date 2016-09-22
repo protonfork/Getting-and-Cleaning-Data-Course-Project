792915a8a0d3ed497854a1de028f64239e302f57
protonfork <thefork@protonmail.com>
2016-09-22 16:35:39 +0200

    final version of R scripts and table sum result

--- datapaths.R
+++ datapaths.R
@@ -1,27 +1,26 @@
 # datapaths.R
 #
-# this script initialize vector which point on the various file 
+# this script initialize vector which point on the data files 
 # of the Human Activity Recognition Using Smartphones Data Set
 #
 
 initDataPath <- function(){
-
-featureinfoPath <<- './data/features_info.txt'# Shows information about the variables used on the feature vector.
-
-featuresPath <<- './data/features.txt'# List of all features.
-
-activitylabelsPath <<- './data/activity_labels.txt'# Links the class labels with their activity name.
-
-xtrainsetPath <<- './data/train/X_train.txt'# Training set.
-
-ytrainPath <<- './data/train/y_train.txt'# Training labels.
-
-subjecttrainPath <<- './data/train/subject_train.txt'# Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
-
-xtestsetPath <<- './data/test/X_test.txt'# Test set.
-
-ytestPath <<- './data/test/y_test.txt'# Test labels.
-
-subjecttestPath <<- './data/test/subject_test.txt'# Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
-
+                # Shows information about the variables used on the feature vector.     
+                featureinfoPath <<- './data/features_info.txt'
+                # List of all features.
+                featuresPath <<- './data/features.txt'
+                # Links the class labels with their activity name.
+                activitylabelsPath <<- './data/activity_labels.txt'
+                # Training set.
+                xtrainsetPath <<- './data/train/X_train.txt'
+                # Training labels.
+                ytrainPath <<- './data/train/y_train.txt'
+                # Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+                subjecttrainPath <<- './data/train/subject_train.txt'
+                # Test set.
+                xtestsetPath <<- './data/test/X_test.txt'
+                # Test labels.
+                ytestPath <<- './data/test/y_test.txt'
+                # Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+                subjecttestPath <<- './data/test/subject_test.txt' 
 }
--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,21 @@
 # run_analysis.R
 #
 # This script execute an analysis of Human Activity Recognition Using Smartphones Data Set
+# The raw dataset provided in two part (Train, Test) and several files is  
+# merged in a single data sets
+# columns are renamed to be suitable variable names
+# finaly, a summary of the dataset, gathering means of measures by 
+# activity and subjects is exported to DataSummary.txt
+#
+# required packages : dplyr
 #
 #
-rm(list=ls())
-source("datapaths.R")
-library(dplyr)
 
-# init file paths
+# loading sources & library
+source("datapaths.R")   # list of path to the raw data set
+library(dplyr)          # library for data frame manangement
+
+# init of the file paths variables [see datapaths.R for definition]
 
 initDataPath()
 
@@ -28,10 +36,8 @@ xtrain <- read.table(xtrainsetPath)
 names(xtrain) <- features[,2]
 
 # Uses descriptive activity names
-ytrain <- read.table(ytrainPath) %>% merge(activities)
+ytrain <- read.table(ytrainPath) %>% inner_join(activities, by = "V1")
 names(ytrain) <- c("ActivityId", "Activity")
-
-trainData <- cbind(subjectstrain, ytrain, xtrain)
 
 # Step 1.4 read test set and set names
 subjectstest <- read.table(subjecttestPath)
@@ -41,35 +47,39 @@ xtest <- read.table(xtestsetPath)
 names(xtest) <- features[,2]
 
 # Uses descriptive activity names
-ytest <- read.table(ytestPath) %>% merge(activities) # Uses descriptive activity names
+ytest <- read.table(ytestPath) %>% inner_join(activities, by = "V1") # Uses descriptive activity names
 names(ytest) <- c("ActivityId", "Activity")
 
-testData <- cbind(subjectstest, ytest, xtest)
-
 # merge the two set into one set of data
+trainData <- cbind(subjectstrain, ytrain, xtrain)
+testData <- cbind(subjectstest, ytest, xtest)
 DataSet <- rbind(testData, trainData)
 
 # Step 2 - Extracts only the measuretments on the mean and standard deviation for each measurement. 
 # Step 2.1 build list of names to keep in the data set
-nameslist <- grep("^[fb]*[Mm]ean|[Ss]td*",names(DataSet))
+nameslist <- grep("(^[^angle])",names(DataSet))
+nameslist <- grep("(([Mm]ean)|([Ss]td))",names(DataSet)[nameslist])
 nameslist <- c(1:3,nameslist)   # column 1:3 being those added from subject & y sets
+# filter dataset columns
 DataSet <- DataSet[nameslist]
 
 # Step 4 - Appropriately labels the data set with descriptive variable names. 
-names(DataSet) <- sub("mean[(][])]", ": Mean",names(DataSet))
-names(DataSet) <- sub("std[(][])]", ": Standard Deviation",names(DataSet))
-names(DataSet) <- sub("^t", "[Time Domain] ",names(DataSet))
-names(DataSet) <- sub("^f", "[Frequency Domain] ",names(DataSet))
-names(DataSet) <- sub("[(][])]", "",names(DataSet))
-names(DataSet) <- sub("[-]", " ",names(DataSet))
-names(DataSet) <- sub("[Bb]ody", "Body ",names(DataSet))
-names(DataSet) <- sub("[Gg]yro", "Gyroscope ",names(DataSet))
-names(DataSet) <- sub("[Aa]cc", "Acceleration ",names(DataSet))
-names(DataSet) <- sub("[Gg]ravity", "Gravity ",names(DataSet))
-names(DataSet) <- sub("JerkMag", "Jerk Magnitude",names(DataSet))
-
+names(DataSet) <- gsub("mean[(][])]", "Mean",names(DataSet))
+names(DataSet) <- gsub("std[(][])]", "StandardDeviation",names(DataSet))
+names(DataSet) <- gsub("[(][])]", "",names(DataSet))
+names(DataSet) <- gsub("[-]", "_",names(DataSet))
+names(DataSet) <- gsub("[Bb]ody", "Body",names(DataSet))
+names(DataSet) <- gsub("[Gg]yro", "Gyroscope",names(DataSet))
+names(DataSet) <- gsub("[Aa]cc", "Acceleration",names(DataSet))
+names(DataSet) <- gsub("[Gg]ravity", "Gravity",names(DataSet))
+names(DataSet) <- gsub("JerkMag", "JerkMagnitude",names(DataSet))
+names(DataSet) <- gsub("BodyBody", "Body",names(DataSet))
 
 # Step 5 - From the data set in step 4, creates a second, independent tidy data set with 
 # the average of each variable for each activity and each subject.
 
-
+tidy <- group_by(DataSet, SubjectId, Activity) %>% 
+        summarize_all(funs(mean)) %>% 
+        arrange(SubjectId, ActivityId)
+        
+write.table(tidy, "./DataSummary.txt", row.names=F, sep="\t")
